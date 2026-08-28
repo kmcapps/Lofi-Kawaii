a364a81b3a7a314333b8c20aec8370acaa2a71c6
kyo-kum <noreply>
2026-08-28 16:58:04 +0900

    fix: support Discord proxy asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Lofi-Kawaii/' : '/',
+  base: command === 'build' ? './' : '/',
   server: {
     host: '::',
   },
